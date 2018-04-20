bc7cdfacade6d7baa88f4fad909afb5452002406
thisi5patrick <thisi5patrick@users.noreply.github.com>
2018-04-20 20:09:04 +0200

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,20 @@
+import View
+import tkinter as tk
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+
+    root.attributes("-fullscreen",True)
+    root.update()
+
+    wyjdz = tk.Button(root, text="Wyjscie", command=root.destroy)
+    wyjdz.place(x=root.winfo_width() - 200, y=root.winfo_height() - 100, width=170, height=50)
+
+
+    View.View(root)
+
+
+    root.mainloop()
+
+
