6a01f7f6b74a9ff5527c342d052fa18be1164cfb
MateuszIce <mateusz.cialowicz@wp.pl>
2018-04-25 21:19:49 +0200

    3 classes added
    Schedule, ScheduleDisplayer, ScheduleHandler

---
+++ Schedule.py
@@ -0,0 +1,10 @@
+class Schedule:
+    ScheduleList = [[0 for x in range(16)] for y in range(35)]
+        pass
+
+    ScheduleDisplayer=Schedule()
+    ScheduleDisplayer.variable = "test"
+    print ScheduleDisplayer.variable
+
+    ScheduleHandler=Schedule()
+        pass
---
+++ ScheduleHandler.py
@@ -0,0 +1,12 @@
+class ScheduleHandler:
+    #def import jest błedne, podkresla
+    def importer(self):
+        pass
+    def export(self):
+        pass
+    def generate(self):
+        pass
+    def accept(self):
+        pass
+    def clear(self):
+        pass
