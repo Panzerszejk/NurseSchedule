13ec2d4e2fd16554b34852ca7204d3ba5d64a2f3
Panzerszejk <bober.konrad@student.pk.edu.pl>
2018-04-25 13:31:13 +0200

    #5 Dodano klase importera i metode do importu

---
+++ Importer.py
@@ -0,0 +1,22 @@
+class Importer:
+
+    def doimport(self):
+        f = open('test.txt', 'r')
+        days = []
+        for line in f:
+            print(line,len(line))
+            if len(line) != 17:
+                print("Blad nie ma danych dla 16 pielegniarek")
+            else:
+                nurse = []
+                for i in range(0, len(line)):
+                    nurse.append(line[i])
+                    
+                days.append(nurse)
+                print(days)
+
+        return days
+
+
+imp = Importer()
+imp.doimport()
