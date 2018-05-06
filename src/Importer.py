72fc00227139573e686b9c0414510d4b6fc07101
Panzerszejk <bober.konrad@student.pk.edu.pl>
2018-05-06 14:55:16 +0200

    Exporter importer menu importu

--- src/Importer.py
+++ src/Importer.py
@@ -1,9 +1,10 @@
 class Importer:
-    def doimport(self):
-        f = open('test.txt', 'r')
+    @staticmethod
+    def doimport(self,filename):
+        f = open(filename, 'r')
         days = []
         for line in f:
-            #print(line,len(line))
+            print(line,len(line))
             if len(line) != 17:
                 print("Blad nie ma danych dla 16 pielegniarek")
             else:
@@ -11,5 +12,9 @@ class Importer:
                 for i in range(0, len(line)):
                     nurse.append(line[i])
                 days.append(nurse)
-                #print(days)
+                print(days)
         return days
+
+
+imp = Importer()
+imp.doimport()
--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,29 @@
 from .View import View
+from tkinter import Menu,filedialog
 import tkinter as tk
 
 
-root = tk.Tk()
-root.attributes("-fullscreen",True)
-root.update()
-wyjdz = tk.Button(root, text="Wyjscie", command=root.destroy)
-wyjdz.place(x=root.winfo_width() - 200, y=root.winfo_height() - 100, width=170, height=50)
-View = View(root)
-root.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
 
-#this file is executed before main and should consist of imports etc. , the above instructions should probably go into main?
+    root.attributes("-fullscreen",True)
+    root.update()
+
+    wyjdz = tk.Button(root, text="Wyjscie", command=root.destroy)
+    wyjdz.place(x=root.winfo_width() - 200, y=root.winfo_height() - 100, width=170, height=50)
+
+    menubar = Menu(root)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Import", command=lambda: )
+    menubar.add_cascade(label="File", menu=filemenu)
+    master.config(menu=menubar)
+
+    View.View(root)
+
+
+    root.mainloop()
+
+
+    def ImportMenu(self):
+        filename =  filedialog.askopenfilename(initialdir = ".",title = "Select file",filetypes = (("txt files","*.txt"),("all files","*.*")))
+        doimport(filename)
