3256845df2dcfe34997a0bfc75bc5a13536e80d0
TheJormungand <sensei.kami@gmail.com>
2018-05-08 15:49:43 +0200

    Fixes to class Schedule and ScheduleHandler

--- src/Schedule.py
+++ src/Schedule.py
@@ -1,3 +1,6 @@
+from .ScheduleHandler import ScheduleHandler
+
+
 class Schedule:
     def __init__(self):
         self.scheduleList = [[0 for x in range(16)] for y in range(35)]   #schedule where we generate data into
--- src/ScheduleHandler.py
+++ src/ScheduleHandler.py
@@ -6,33 +6,42 @@ from .Exporter import Exporter
 
 
 class ScheduleHandler:
+
+    imported = None
+
+    @staticmethod
     def importer(self, var):
         imp = Importer()
         if var == "week":
             sched = Week()
             sched.importedWeek = imp.doimport()
+            ScheduleHandler.imported = sched
         elif var == "schedule":
             sched = Schedule()
             sched.importedSchedule = imp.doimport()
+            ScheduleHandler.imported = sched
         else:
             sched = None
             print("No argument given")
-        return sched
 
+    @staticmethod
     def export(self, sched):  #sched is a class Schedule object
         exp = Exporter()
         exp.export(sched)
 
-    def generate(self, ward, imported = None):  #imported is a class Schedule or Week object, can be Null
+    @staticmethod
+    def generate(self, ward):  #imported is a class Schedule or Week object, can be Null
         gen = Generator()
-        if imported is None:
+        if ScheduleHandler.imported is None:
             return gen.generate(ward)
-        else:
-            return gen.generate(ward,imported)
+        elif isinstance(ScheduleHandler.imported, Week) or isinstance(ScheduleHandler.imported,Schedule):
+            return gen.generate(ward,ScheduleHandler.imported)
 
+    @staticmethod
     def accept(self):
         pass
 
+    @staticmethod
     def clear(self):
         pass
 
