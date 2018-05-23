b5a1783bbe4d151325c5cdba0505925e59d59afc
MateuszIce <mateusz.cialowicz@wp.pl>
2018-05-23 23:30:35 +0200

    v3
    edit to ok

--- Nurse_test.py
+++ Nurse_test.py
@@ -4,29 +4,27 @@ from src.Nurse import Nurse
 
 class MyTestCase(unittest.TestCase):
     def test_number(self):
-        self.nurse_test = Nurse(self, 1, 'Janina', 'Bak', 5, 2, True)
+        self.nurse_test = Nurse(1, 'Janina', 'Bak', 5, 2, True)
         self.assertEqual(self.nurse_test.number, 1)
 
     def test_name(self):
-     self.nurse_test = Nurse(self, 1, 'Janina', 'Bak', 5, 2, True)
-     self.assertEqual(self.nurse_test.name, 'Janina')
+        self.nurse_test = Nurse(1, 'Janina', 'Bak', 5, 2, True)
+        self.assertEqual(self.nurse_test.name, 'Janina')
 
     def test_surname(self):
-        self.nurse_test = Nurse(self, 1, 'Janina', 'Bak', 5, 2, True)
+        self.nurse_test = Nurse(1, 'Janina', 'Bak', 5, 2, True)
         self.assertEqual(self.nurse_test.surname, 'Bak')
 
-
-    def test_maxshifts(self):
-        self.nurse_test = Nurse(self, 1, 'Janina', 'Bak', 5, 2, True)
+    def test_max(self):
+        self.nurse_test = Nurse(1, 'Janina', 'Bak', 5, 2, True)
         self.assertEqual(self.nurse_test.maxShifts, 5)
 
-
-    def test_minshifts(self):
-        self.nurse_test = Nurse(self, 1, 'Janina', 'Bak', 5, 2, True)
+    def test_min(self):
+        self.nurse_test = Nurse(1, 'Janina', 'Bak', 5, 2, True)
         self.assertEqual(self.nurse_test.minShifts, 2)
 
-    def test_nightshifts(self):
-        self.nurse_test = Nurse(self, 1, 'Janina', 'Bak', 5, 2, True)
+    def test_night(self):
+        self.nurse_test = Nurse(1, 'Janina', 'Bak', 5, 2, True)
         self.assertEqual(self.nurse_test.workingNights, True)
 
 if __name__ == '__main__':
