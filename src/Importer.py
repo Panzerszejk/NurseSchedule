6b221e99df37e83b44c5d9d34d5976dcff263882
pawarm <piwirm@gmail.com>
2018-05-23 22:21:04 +0200

    Importer updated to different formats

--- src/Importer.py
+++ src/Importer.py
@@ -6,12 +6,15 @@ class Importer:
         f = open(filename, 'r')
         days = []
         for line in f:
-            if len(line) != 17:
-                print("Blad nie ma danych dla 16 pielegniarek")
-            else:
+            if len(line) in [17,8,36]:
                 nurse = []
                 for i in range(0, len(line)-1):
                     nurse.append(line[i])
                 days.append(nurse)
+            else:
+                print("Nieprawidlowy format danych")
+
         f.close()
+        if len(days) == 16:
+            days = [list(i) for i in zip(*days)]
         return days
